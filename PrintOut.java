f2022b8358c8051006f891721da1cc3403f9ec1e
yikai <ypeng83@wisc.edu>
2024-06-16 19:30:02 -0500

    New changes to PrintOut.java

--- PrintOut.java
+++ PrintOut.java
@@ -1,6 +1,6 @@
 // A test file for printing out a sentence
 public class Printout {
 	public static void main(String[] args){
-		System.out.println("Test");
+		System.out.println("Test and more");
 	}
 }
